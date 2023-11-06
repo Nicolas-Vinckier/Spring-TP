f88bd9826a68cdfafae6683adb89657bb23d01c5
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-11-06 10:26:18 +0100

    :tada: New spring project

---
+++ tp/src/main/java/fr/diginamic/tp/TpApplication.java
@@ -0,0 +1,14 @@
+package fr.diginamic.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
