35d8212242d9bee65b64b4af6a11539ca672899e
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-11-06 10:44:34 +0100

    :bug: Résolution

--- tp/src/main/java/fr/diginamic/tp/TpApplication.java
+++ tp/src/main/java/fr/diginamic/tp/TpApplication.java
@@ -2,13 +2,20 @@ package fr.diginamic.tp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpApplication.class, args);
-		System.out.println("Hello World");
+	}
+
+	@GetMapping("hello")
+	public String hello() {
+		return "Hello World !";
 	}
 
 }
