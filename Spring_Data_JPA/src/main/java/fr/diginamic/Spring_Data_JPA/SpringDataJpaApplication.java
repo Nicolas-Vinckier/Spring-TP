f308f83c2115608100be15f9c8bc58d48ffb016f
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-11-07 16:28:35 +0100

    💡 Mise en commentaire des fonctions

--- Spring_Data_JPA/src/main/java/fr/diginamic/Spring_Data_JPA/SpringDataJpaApplication.java
+++ Spring_Data_JPA/src/main/java/fr/diginamic/Spring_Data_JPA/SpringDataJpaApplication.java
@@ -67,48 +67,64 @@ public class SpringDataJpaApplication implements CommandLineRunner {
 		// // Retrouver des animaux par leur couleur (avec la méthode findByColor)
 		// System.out.println(animalRepository.findByColor("Roux"));
 
-		// Avec la méthode findAllByOrderByCommonNameAsc (qui utilise une Query),
-		// retournez toutes les espèces triées par ordre alphabétique de nom commun.
-		System.out.println("===================== findAllByOrderByCommonNameAsc =====================");
-		System.out.println(speciesRepository.findAllByOrderByCommonNameAsc());
-		System.out.println("=========================================================================");
+		// // Avec la méthode findAllByOrderByCommonNameAsc (qui utilise une Query),
+		// // retournez toutes les espèces triées par ordre alphabétique de nom commun.
+		// System.out.println("===================== findAllByOrderByCommonNameAsc
+		// =====================");
+		// System.out.println(speciesRepository.findAllByOrderByCommonNameAsc());
+		// System.out.println("=========================================================================");
 
-		// Avec la méthode findByCommonNameLike (qui utilise une Query), retournez
-		// toutes les espèces dont le nom commun contient le mot "xxx".
-		System.out.println("===================== findByCommonNameLike =====================");
-		System.out.println(speciesRepository.findByCommonNameLike("Chien"));
-		System.out.println("================================================================");
+		// // Avec la méthode findByCommonNameLike (qui utilise une Query), retournez
+		// // toutes les espèces dont le nom commun contient le mot "xxx".
+		// System.out.println("===================== findByCommonNameLike
+		// =====================");
+		// System.out.println(speciesRepository.findByCommonNameLike("Chien"));
+		// System.out.println("================================================================");
 
-		// Avec la méthode findByAgeBetween, retournez toutes les personnes dont l'âge
-		// est entre xx et xx ans.
-		System.out.println("===================== findByAgeBetween =====================");
-		System.out.println(personRepository.findByAgeBetween(18, 30));
-		System.out.println("============================================================");
+		// // Avec la méthode findByAgeBetween, retournez toutes les personnes dont
+		// l'âge
+		// // est entre xx et xx ans.
+		// System.out.println("===================== findByAgeBetween
+		// =====================");
+		// System.out.println(personRepository.findByAgeBetween(18, 30));
+		// System.out.println("============================================================");
 
-		// Avec la méthode findByAnimal, retournez toutes les personnes qui possèdent un
-		// animal donné
-		System.out.println("===================== findByAnimal =====================");
-		System.out.println(personRepository.findByAnimal(animalRepository.findById(1).orElseThrow()));
-		System.out.println("========================================================");
+		// // Avec la méthode findByAnimal, retournez toutes les personnes qui possèdent
+		// un
+		// // animal donné
+		// System.out.println("===================== findByAnimal
+		// =====================");
+		// System.out.println(personRepository.findByAnimal(animalRepository.findById(1).orElseThrow()));
+		// System.out.println("========================================================");
 
-		// Avec la méthode countBySex, retournez le nombre d'animaux du sexe donné
-		System.out.println("===================== countBySex =====================");
-		System.out.println(animalRepository.countBySex(Sex.M));
-		System.out.println(animalRepository.countBySex(Sex.F));
-		System.out.println("======================================================");
+		// // Avec la méthode countBySex, retournez le nombre d'animaux du sexe donné
+		// System.out.println("===================== countBySex =====================");
+		// System.out.println(animalRepository.countBySex(Sex.M));
+		// System.out.println(animalRepository.countBySex(Sex.F));
+		// System.out.println("======================================================");
 
-		// Avec la méthode hasOwner, retournez true si l'animal donné a un propriétaire
-		System.out.println("===================== hasOwner =====================");
-		System.out.println("ID : 1 => " + animalRepository.hasOwner(animalRepository.findById(1).orElseThrow()));
-		System.out.println("ID : 2 => " + animalRepository.hasOwner(animalRepository.findById(2).orElseThrow()));
-		System.out.println("ID : 3 => " + animalRepository.hasOwner(animalRepository.findById(3).orElseThrow()));
-		System.out.println("ID : 4 => " + animalRepository.hasOwner(animalRepository.findById(4).orElseThrow()));
-		System.out.println("ID : 5 => " + animalRepository.hasOwner(animalRepository.findById(5).orElseThrow()));
-		System.out.println("ID : 6 => " + animalRepository.hasOwner(animalRepository.findById(6).orElseThrow()));
-		System.out.println("ID : 7 => " + animalRepository.hasOwner(animalRepository.findById(7).orElseThrow()));
-		System.out.println("ID : 8 => " + animalRepository.hasOwner(animalRepository.findById(8).orElseThrow()));
-		System.out.println("ID : 9 => " + animalRepository.hasOwner(animalRepository.findById(9).orElseThrow()));
-		System.out.println("====================================================");
+		// // Avec la méthode hasOwner, retournez true si l'animal donné a un
+		// propriétaire
+		// System.out.println("===================== hasOwner =====================");
+		// System.out.println("ID : 1 => " +
+		// animalRepository.hasOwner(animalRepository.findById(1).orElseThrow()));
+		// System.out.println("ID : 2 => " +
+		// animalRepository.hasOwner(animalRepository.findById(2).orElseThrow()));
+		// System.out.println("ID : 3 => " +
+		// animalRepository.hasOwner(animalRepository.findById(3).orElseThrow()));
+		// System.out.println("ID : 4 => " +
+		// animalRepository.hasOwner(animalRepository.findById(4).orElseThrow()));
+		// System.out.println("ID : 5 => " +
+		// animalRepository.hasOwner(animalRepository.findById(5).orElseThrow()));
+		// System.out.println("ID : 6 => " +
+		// animalRepository.hasOwner(animalRepository.findById(6).orElseThrow()));
+		// System.out.println("ID : 7 => " +
+		// animalRepository.hasOwner(animalRepository.findById(7).orElseThrow()));
+		// System.out.println("ID : 8 => " +
+		// animalRepository.hasOwner(animalRepository.findById(8).orElseThrow()));
+		// System.out.println("ID : 9 => " +
+		// animalRepository.hasOwner(animalRepository.findById(9).orElseThrow()));
+		// System.out.println("====================================================");
 
 	}
 
