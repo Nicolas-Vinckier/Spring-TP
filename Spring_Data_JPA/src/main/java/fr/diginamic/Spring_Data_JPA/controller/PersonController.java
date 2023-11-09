24c2e4891eca769d981be141f5cc28f672269771
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-11-09 10:02:46 +0100

    :package: Add Person controller for handling requests related to person data

--- Spring_Data_JPA/src/main/java/fr/diginamic/Spring_Data_JPA/controller/PersonController.java
+++ Spring_Data_JPA/src/main/java/fr/diginamic/Spring_Data_JPA/controller/PersonController.java
@@ -1,5 +1,10 @@
 package fr.diginamic.Spring_Data_JPA.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/person")
 public class PersonController {
-    
+
 }
