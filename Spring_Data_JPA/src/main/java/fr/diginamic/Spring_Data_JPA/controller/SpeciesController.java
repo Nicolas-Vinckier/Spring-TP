4c7219d8527184ba8c52cde37884802e0e636c84
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-11-09 10:01:00 +0100

    :construction: Add controller for managing species

--- Spring_Data_JPA/src/main/java/fr/diginamic/Spring_Data_JPA/controller/SpeciesController.java
+++ Spring_Data_JPA/src/main/java/fr/diginamic/Spring_Data_JPA/controller/SpeciesController.java
@@ -1,5 +1,45 @@
 package fr.diginamic.Spring_Data_JPA.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import fr.diginamic.Spring_Data_JPA.model.Species;
+
+import fr.diginamic.Spring_Data_JPA.repository.SpeciesRepository;
+
+@Controller
 public class SpeciesController {
-    
+
+    @Autowired
+    private SpeciesRepository speciesRepository;
+
+    @GetMapping("/species")
+    public String listSpecies(Model model) {
+        List<Species> species = (List<Species>) speciesRepository.findAll();
+        model.addAttribute("species", species);
+        return "list_species";
+    }
+
+    @GetMapping("/species/{id}")
+    public String initUpdate(@PathVariable("id") Integer id, Model model) {
+        Optional<Species> species = speciesRepository.findById(id);
+        if (species.isPresent()) {
+            model.addAttribute(species.get());
+            return "update_species";
+        }
+        return "error";
+    }
+
+    @GetMapping("/species/create")
+    public String initCreate(Model model) {
+        model.addAttribute(new Species());
+        return "create_species";
+    }
+
 }
