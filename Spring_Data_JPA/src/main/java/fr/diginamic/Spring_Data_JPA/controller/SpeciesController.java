9f32efd9c63e1db44096a4ab8287985cd779622f
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-11-09 13:41:34 +0100

    :recycle: Refactor SpeciesController to improve readability and code structure

--- Spring_Data_JPA/src/main/java/fr/diginamic/Spring_Data_JPA/controller/SpeciesController.java
+++ Spring_Data_JPA/src/main/java/fr/diginamic/Spring_Data_JPA/controller/SpeciesController.java
@@ -4,32 +4,34 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.diginamic.Spring_Data_JPA.model.Species;
 
 import fr.diginamic.Spring_Data_JPA.repository.SpeciesRepository;
 
 @Controller
+@RequestMapping("/species")
 public class SpeciesController {
 
     @Autowired
     private SpeciesRepository speciesRepository;
 
-    @GetMapping("/species")
+    @GetMapping("/")
     public String listSpecies(Model model) {
         List<Species> species = speciesRepository.findAll();
-        model.addAttribute("species", species);
+        model.addAttribute("speciesList", species);
+
         return "list_species";
     }
 
-    @GetMapping("/species/{id}")
+    @GetMapping("/{id}")
     public String initUpdate(@PathVariable("id") Integer id, Model model) {
         Optional<Species> species = speciesRepository.findById(id);
         if (species.isPresent()) {
@@ -39,16 +41,10 @@ public class SpeciesController {
         return "error";
     }
 
-    @GetMapping("/species/create")
+    @GetMapping("/create")
     public String initCreate(Model model) {
         model.addAttribute(new Species());
         return "create_species";
-    }
-
-    @GetMapping("/species")
-    public String showSpecies(Model model) {
-        model.addAttribute("speciesList", speciesRepository.findAll(Sort.by(Sort.Direction.ASC, "commonName")));
-        return "list_species";
     }
 
     // @PostMapping("/species")
@@ -56,13 +52,13 @@ public class SpeciesController {
     // this.speciesRepository.save(speciesItem);
     // return "redirect:/species";
     // }
-    @PostMapping("/species")
+    @PostMapping("/")
     public String createOrUpdate(@RequestBody Species speciesItem) {
         this.speciesRepository.save(speciesItem);
         return "redirect:/species";
     }
 
-    @GetMapping("/species/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String delete(@PathVariable("id") Integer speciesId) {
         Optional<Species> speciesToDelete = this.speciesRepository.findById(speciesId);
         speciesToDelete.ifPresent(species -> this.speciesRepository.delete(species));
